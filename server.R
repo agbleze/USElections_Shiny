d335ebf246f3540eeb80c5b99c048edfefc9d291
Agbleze <62272559+agbleze@users.noreply.github.com>
2021-08-22 14:05:48 +0200

    Update server.R

--- server.R
+++ server.R
@@ -5,25 +5,6 @@ library(shiny)
 library(tidyverse)
 library(socviz)
 
-tags$head(
-        HTML(
-          "
-          <script>
-          var socket_timeout_interval
-          var n = 0
-          $(document).on('shiny:connected', function(event) {
-          socket_timeout_interval = setInterval(function(){
-          Shiny.onInputChange('count', n++)
-          }, 15000)
-          });
-          $(document).on('shiny:disconnected', function(event) {
-          clearInterval(socket_timeout_interval)
-          });
-          </script>
-          "
-        )
-        )
-
 server <- function(input, output, session){
     
     theData <- reactive({
